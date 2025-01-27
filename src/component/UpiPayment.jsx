f295fe230049f8b6f04a3f5dc7663b16be5d8e62
Ashu <bazimaargames@gmail.com>
2025-01-27 17:34:12 +0530

    changes

--- src/component/UpiPayment.jsx
+++ src/component/UpiPayment.jsx
@@ -29,7 +29,7 @@ function UpiPayment() {
 
     try {
       const statusResponse = await axios.post('https://paymentapibackend.onrender.com/api/order/create', {
-        token: '225e3b-5843ec-ddb76d-a14f84-5c4741',
+        token: 'b93b87-7195bc-2f74f2-29903f-930a8c',
         order_id: orderId,
       });
 
